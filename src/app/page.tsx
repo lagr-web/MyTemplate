e3f0f2793059b6c1ba2f4b38441bf90e40e6e7e6
Lars Gregersen <lagr@videndjurs.dk>
2024-02-17 13:43:29 +0100

    template update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { Category, cat } from "@/type/test";
 import { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
 
-
 const Page = () => {
 
   const phoneNumber = '123456';
@@ -36,53 +35,6 @@ const Page = () => {
         ))
       }
 
-
-      {/* 
-      <section className="z-0 container mx-auto my-10">
-        <div className="w-20 bg-black text-white font-bold m-4 text-center rounded-lg md:mx-0 sm:mx-5 lg:mx-2 xl:mx-20 text-xl">Jobs</div>
-        <div className="cardwrapper">
-
-          {data.job &&
-            data.job.map((item: Content, index: number) => (
-              <Card key={item.id} data={item} />
-            ))
-          }
-        </div>
-      </section>
-
-
-      <section className="z-0 container mx-auto my-10">
-        <div className="w-20 bg-black text-white font-bold m-4 text-center rounded-lg md:mx-0 sm:mx-5 lg:mx-2 xl:mx-20 text-xl">Skills</div>
-        <div className="cardwrapper">
-
-          {data.skills &&
-            data.skills.map((item: Content, index: number) => (
-
-              <Card key={item.id} data={item} />
-
-            ))
-
-          }
-
-        </div>
-      </section>
-
-      <section className="z-0 container mx-auto my-10">
-        <div className="w-20 bg-black text-white font-bold m-4 text-center rounded-lg md:mx-0 sm:mx-5 lg:mx-2 xl:mx-20 text-xl">Fun</div>
-        <div className="cardwrapper">
-
-          {data.fun &&
-            data.fun.map((item: Content, index: number) => (
-
-              <Card key={item.id} data={item} />
-
-            ))
-
-          }
-
-        </div>
-      </section>
- */}
       <footer className="bg-black text-white p-1 text-center">
         mail:manglerbarelige@gmail.com | mobil:{phoneLink}
       </footer>
